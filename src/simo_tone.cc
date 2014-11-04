4dc328d797f428000c030257eec12209eeda024d
Manu T S <manu.ts@outlook.com>
2014-11-04 10:25:10 +0100

    modifying simo_tone.cc 	modified:   src/simo_tone.cc     --     Idea is to make one code with tx and rx usrps. Till now I have successfully     generated samples of a given frequency using the liquid's nco_crcf object

--- src/simo_tone.cc
+++ src/simo_tone.cc
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 #include <complex>
 #include <getopt.h>
+#include <liquid/liquid.h>
 
 #include <uhd/usrp/multi_usrp.hpp>
 
@@ -29,139 +30,83 @@ void usage() {
     printf("miso_tone -- transmit tones in MISO channel\n");
     printf("\n");
     printf("  u,h   : usage/help\n");
-    printf("  f     : center frequency [Hz], default: 900 MHz\n");
-    printf("  s     : usrp sampling rate [Hz] default: 1 MHz\n");
-    printf("  g     : software rx gain [dB] (default: -6dB)\n");
-    printf("  G     : uhd rx gain [dB] (default: 40dB)\n");
+    printf("  F     : center frequency [Hz], default: 900 MHz\n");
+    printf("  f     : tone frequency [Hz], default: 1 kHz\n");
+    printf("  r     : usrp sampling rate [Hz] default: 1 MHz\n");
+    printf("  a     : tone amplitude (default: 0.25)\n");
+    printf("  G     : uhd rx gain [dB] (default: 20dB)\n");
     printf("  S     : number of samples to capture (default: 2M)\n");
 }
 
 int main (int argc, char **argv)
 {
-    // defining PI
-    double PI = std::acos(-1);
+  // define temp variables
+  FILE * sink;
+  sink = fopen("/tmp/sink", "wb");
+  if (!(sink)){
+    std::cout << "File open failed\n";
+    exit(1);
+  }
+  // defining PI
+  double PI = std::acos(-1);
+  
+  double cent_freq = 900.0e6;         // center frequency of transmission
+  double tone_freq = 1000.0;          // tone frequency
+  double samp_rate = 1e6;             // usrp samping rate
+  double tone_amp = 0.25;             // tone amplitude
+  double uhd_rxgain = 20.0;           // rx frontend gain
+  double num_samps = 2e6;             // number of samples to capture.
+  size_t sig_src_buff_len = 1e6;      // size of the signal source buffer
 
-    double frequency = 900.0e6;
-    double samp_rate = 2.0e5;
-    double rxgain = 0.25;               // software rx gain [dB]
-    double uhd_rxgain = 20.0;           // uhd (hardware) rx gain
-    double num_samples = 2000;           // uhd (hardware) rx gain
-
-    int d;
-    while ((d = getopt(argc,argv,"uhf:s:g:G:S:")) != EOF) {
-        switch (d) {
-        case 'u':
-        case 'h':   usage();                        return 0;
-        case 'f':   frequency   = atof(optarg);     break;
-        case 's':   samp_rate   = atof(optarg);     break;
-        case 'g':   rxgain      = atof(optarg);     break;
-        case 'G':   uhd_rxgain  = atof(optarg);     break;
-        case 'S':   num_samples = atof(optarg);     break;
-        default:
-            usage();
-            return 0;
-        }
+  int d;
+  while ((d = getopt(argc,argv,"uhF:f:r:a:G:S:")) != EOF) {
+    switch (d) {
+      case 'u':
+      case 'h':   usage();                        return 0;
+      case 'F':   cent_freq   = atof(optarg);     break;
+      case 'f':   tone_freq   = atof(optarg);     break;
+      case 'r':   samp_rate   = atof(optarg);     break;
+      case 'a':   tone_amp    = atof(optarg);     break;
+      case 'G':   uhd_rxgain  = atof(optarg);     break;
+      case 'S':   num_samps   = atof(optarg);     break;
+      default:
+                  usage();                        return 0;
     }
+  }
 
+    // define the address for tx and rx
+    uhd::device_addr_t tx_addr, rx_addr;
+    tx_addr["addr0"] = "134.147.118.211";
+    rx_addr["addr0"] = "134.147.118.213";
+    uhd::usrp::multi_usrp::sptr tx = uhd::usrp::multi_usrp::make(tx_addr);
+    uhd::usrp::multi_usrp::sptr rx = uhd::usrp::multi_usrp::make(rx_addr);
 
-    uhd::device_addr_t dev_addr;
-    // device address in dks network
-    dev_addr["addr0"] = "134.147.118.213";
-    dev_addr["addr1"] = "134.147.118.214";
-    uhd::usrp::multi_usrp::sptr usrp = uhd::usrp::multi_usrp::make(dev_addr);
-
-    // declaring the MIMO config
-    usrp->set_clock_source("mimo", 1);
-    usrp->set_time_source("mimo", 1);
-    usrp->set_time_now(uhd::time_spec_t(0.0), 0);
-
-    // waiting for a while
-    std::cout << "Entering sleep\n";
-    sleep(2);
-    std::cout << "Exiting sleep\n";
-
-    // try to set rx rate
-    usrp->set_rx_rate(samp_rate);
-    // get actual rx rate
-    std::cout << "Requested sampling rate\t:" << samp_rate << std::endl;
-    std::cout << "Actual sampling rate\t:" << usrp->get_rx_rate() << std::endl;
-
-    usrp->set_rx_freq(frequency, 0);
-    usrp->set_rx_freq(frequency, 1);
-    usrp->set_rx_gain(uhd_rxgain, 0);
-    usrp->set_rx_gain(uhd_rxgain, 1);
-
-    // creating the streamer
-    std::vector<size_t> channels(2);
-    channels[0] = (size_t)0;
-    channels[1] = (size_t)1;
-    uhd::stream_args_t stream_args("fc32", "sc16");
-    stream_args.channels = channels;
-    uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(stream_args);
-
-    // Length of usrp_buffer
-    const size_t max_buff_len = rx_stream->get_max_num_samps();
-    size_t buff_len = 256;         // TODO add option to enter the buffer length
-    while(buff_len > max_buff_len) {
-        buff_len = buff_len/2;
+    // define the signal source - liquid's nco object
+    // LIQUID_VCO generates complex sinusoids using sinf and cosf
+    // from the standard math library.
+    nco_crcf sig_src = nco_crcf_create(LIQUID_VCO);
+    std::cout << 2*PI*(tone_freq/samp_rate) << std::endl;
+    nco_crcf_set_frequency(sig_src, 2*PI*(tone_freq/samp_rate));
+    // allocate memory to store the sinusoid samples
+    std::complex<float> * sig_src_buff;
+    sig_src_buff = (std::complex<float> *)malloc(sig_src_buff_len*sizeof(std::complex<float>));
+    // populate the signal source
+    for (int i = 0; i < sig_src_buff_len; i++)
+    {
+      nco_crcf_cexpf(sig_src, sig_src_buff + i);
+      nco_crcf_step(sig_src);
     }
-    std::cout << "Maximum possible buffer length\t:" << max_buff_len << std::endl;
-    std::cout << "Allocated buffer length\t\t:" << buff_len << std::endl;
-    // usrp buffer
-    std::vector<std::vector<std::complex<float> > > buffs(
-            usrp->get_rx_num_channels(),
-            std::vector<std::complex<float> >(buff_len)
-            );
-
-    //create a vector of pointers to point to each of the channel buffers
-    std::vector<std::complex<float> *> buff_ptrs;
-    for (size_t i = 0; i < buffs.size(); i++)
-        buff_ptrs.push_back(&buffs[i].front());
-    
-    uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
-    stream_cmd.num_samps = num_samples;
-    stream_cmd.stream_now = true;
-    stream_cmd.time_spec = uhd::time_spec_t();
-    rx_stream->issue_stream_cmd(stream_cmd);
-    uhd::rx_metadata_t md;
-
-    size_t num_acc_samps = 0;
-    size_t num_samps_read;
-
-    double timeout = 1;
-
-    // files to write data to
-    std::ofstream sink1;
-    sink1.open("/tmp/sink1", std::ios::binary);
-    std::ofstream sink2;
-    sink2.open("/tmp/sink2", std::ios::binary);
-
-    while(num_acc_samps < num_samples) {       // FIXME Loop based on time elapsed
-        // Filling the usrp buffer
-        std::cout << "Inside while\n";
-        num_samps_read = rx_stream->recv(buff_ptrs,
-                                         buff_len,
-                                         md,
-                                         3.0,
-                                         true);
-        //handle the error code
-        if (md.error_code == uhd::rx_metadata_t::ERROR_CODE_TIMEOUT){
-            std::cout << "Error Timout\n";
-            break;
-        }
-        if (md.error_code != uhd::rx_metadata_t::ERROR_CODE_NONE){
-            throw std::runtime_error(str(boost::format(
-                "Receiver error %s"
-            ) % md.strerror()));
-        }
-        num_acc_samps += num_samps_read;
-        std::cout << "Writing\n";
-        sink1.write(reinterpret_cast<const char*>(buff_ptrs[0]), buff_len*sizeof(std::complex<float>));
-        sink2.write(reinterpret_cast<const char*>(buff_ptrs[1]), buff_len*sizeof(std::complex<float>));
+    // temp - write samples to file to see in matplotlib
+    if (sig_src_buff_len != fwrite((void *)sig_src_buff,
+                                   sizeof(std::complex<float>),
+                                   sig_src_buff_len,
+                                   sink))
+    {
+      std::cout << "Error in writing to file, exiting\n";
+      exit(1);
     }
-    sink1.close();
-    sink2.close();
-    std::cout << "Number of samples read :" << num_acc_samps;
+    fclose(sink);
+    free(sig_src_buff);
     return 0;
 }
 
