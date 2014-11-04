2b2cb93a8acc58984cc03402b5546baf89abfcd8
Manu T S <manu.ts@outlook.com>
2014-11-04 17:58:12 +0100

    adding tx_info to usrp_info 	modified:   usrp_info.cc     added instructions to print out the transmitter information as well.

--- src/usrp_info.cc
+++ src/usrp_info.cc
@@ -42,7 +42,8 @@ int main (int argc, char **argv)
 
   // define the address for tx and rx
   uhd::device_addr_t tx_addr, rx_addr;
-  tx_addr["addr0"] = "134.147.118.211";
+  tx_addr["addr0"] = "134.147.118.211"; 
+  tx_addr["addr1"] = "134.147.118.212"; 
   rx_addr["addr0"] = "134.147.118.213";
   rx_addr["addr1"] = "134.147.118.214";
   uhd::usrp::multi_usrp::sptr tx = uhd::usrp::multi_usrp::make(tx_addr);
@@ -50,30 +51,55 @@ int main (int argc, char **argv)
 
   rx->set_rx_antenna("TX/RX");
   uhd::dict<std::string, std::string> rx_info_0 = rx->get_usrp_rx_info(0);
-  std::vector<std::string> keys_0(rx_info_0.keys());
-  std::vector<std::string> vals_0(rx_info_0.vals());
-  std::cout << "Channel 0\n----------\n";
+  std::vector<std::string> rx_keys_0(rx_info_0.keys());
+  std::vector<std::string> rx_vals_0(rx_info_0.vals());
+  std::cout << "Receive Channel 0\n----------\n";
   for(size_t n = 0; n < rx_info_0.size(); n++)
   {
     if (n == 3)
-      std::cout << keys_0[n] << "\t\t: " << vals_0[n] << std::endl;
+      std::cout << rx_keys_0[n] << "\t\t: " << rx_vals_0[n] << std::endl;
     else
-      std::cout << keys_0[n] << "\t: " << vals_0[n] << std::endl;
+      std::cout << rx_keys_0[n] << "\t: " << rx_vals_0[n] << std::endl;
   }
   std::cout << "----------\n";
   uhd::dict<std::string, std::string> rx_info_1 = rx->get_usrp_rx_info(1);
-  std::vector<std::string> keys_1(rx_info_1.keys());
-  std::vector<std::string> vals_1(rx_info_1.vals());
-  std::cout << "Channel 1\n----------\n";
+  std::vector<std::string> rx_keys_1(rx_info_1.keys());
+  std::vector<std::string> rx_vals_1(rx_info_1.vals());
+  std::cout << "Receive Channel 1\n----------\n";
   for(size_t n = 0; n < rx_info_1.size(); n++)
   {
     if (n == 3)
-      std::cout << keys_1[n] << "\t\t: " << vals_1[n] << std::endl;
+      std::cout << rx_keys_1[n] << "\t\t: " << rx_vals_1[n] << std::endl;
     else
-      std::cout << keys_1[n] << "\t: " << vals_1[n] << std::endl;
+      std::cout << rx_keys_1[n] << "\t: " << rx_vals_1[n] << std::endl;
   }
   std::cout << "----------\n";
-  std::cout << rx->get_pp_string();
+
+  tx->set_tx_antenna("TX/RX");
+  uhd::dict<std::string, std::string> tx_info_0 = tx->get_usrp_tx_info(0);
+  std::vector<std::string> tx_keys_0(tx_info_0.keys());
+  std::vector<std::string> tx_vals_0(tx_info_0.vals());
+  std::cout << "Transmit Channel 0\n----------\n";
+  for(size_t n = 0; n < tx_info_0.size(); n++)
+  {
+    if (n == 3)
+      std::cout << tx_keys_0[n] << "\t\t: " << tx_vals_0[n] << std::endl;
+    else
+      std::cout << tx_keys_0[n] << "\t: " << tx_vals_0[n] << std::endl;
+  }
+  std::cout << "----------\n";
+  uhd::dict<std::string, std::string> tx_info_1 = tx->get_usrp_tx_info(1);
+  std::vector<std::string> tx_keys_1(tx_info_1.keys());
+  std::vector<std::string> tx_vals_1(tx_info_1.vals());
+  std::cout << "Transmit Channel 1\n----------\n";
+  for(size_t n = 0; n < tx_info_1.size(); n++)
+  {
+    if (n == 3)
+      std::cout << tx_keys_1[n] << "\t\t: " << tx_vals_1[n] << std::endl;
+    else
+      std::cout << tx_keys_1[n] << "\t: " << tx_vals_1[n] << std::endl;
+  }
+  std::cout << "----------\n";
   return 0;
 }
 
